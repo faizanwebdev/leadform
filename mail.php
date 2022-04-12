9df6b3b3869e33b6b50d5326cbc188960fa4f576
Faizan Kazi <79440289+faizanwebdev@users.noreply.github.com>
2022-04-12 12:17:31 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -40,7 +40,7 @@ if(isset($_POST['signup']) && $_SERVER['REQUEST_METHOD'] == "POST"){
             $mail->Host = 'smtp.gmail.com';   
             $mail->SMTPAuth = true;     
             $mail->Username = 'enable.onl@gmail.com';        
-            $mail->Password = 'Enable@22';    
+            $mail->Password = '';    
             $mail->SMTPSecure = 'tls';   
             $mail->Port = 587;
             $mail->setFrom('info@enable.onl', 'ENLYFT');
